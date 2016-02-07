27ca47f1a18aec576a50598fd0e2575df3d872ed
Marc McHardy <marc@marcmchardy.com>
2016-02-07 17:32:59 +1300

    maintainance yo

--- dist/assets/js/app.js
+++
@@ -1,3 +0,0 @@
-$(document).foundation();
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLmZvdW5kYXRpb24oKTtcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
